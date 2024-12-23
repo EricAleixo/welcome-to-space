3a3cd55327f3483dc80e01d7628ea45770aa80c5
ericAleixo <ericaleixo66@gmail.com>
2024-12-23 09:25:28 -0300

    ADD: database

--- src/app/destinos/page.tsx
+++ src/app/destinos/page.tsx
@@ -1,14 +1,16 @@
 "use client"
 import { Header } from "@/components/Header";
-import imagemLua from "@/../public/assets/destination/image-moon.png"
 import Image from "next/image";
 import { useState } from "react";
+import database from "@/database/data.json"
 
 export default function Destinos() {
 
+    const dataPlanetaDestinations = database.destinations;
+
     const [activatePlanet, setActivatePlanet] = useState(0);
 
-    const handlePlanetClick = (index: number) =>{
+    const handlePlanetClick = (index: number) => {
         setActivatePlanet(index)
     }
 
@@ -27,7 +29,7 @@ export default function Destinos() {
                     </h2>
                 </div>
                 <div className="px-36 w-full flex justify-between items-center pr-24">
-                    <Image src={imagemLua} alt={"Imagem da lua"} />
+                    <Image src={dataPlanetaDestinations[activatePlanet].images.png} width={500} height={500} alt={"Imagem da lua"} />
 
                     <div className="w-[50%] flex flex-col gap-10">
                         <ul className={`flex gap-10`}>
@@ -35,7 +37,7 @@ export default function Destinos() {
                                 planetas.map((nomePlaneta, index) => (
                                     <li key={index}
                                         className={`cursor-pointer capitalize ${activatePlanet === index ? "text-white" : "text-[#D0D6F9] "} group`}
-                                        onClick = {() => handlePlanetClick(index)}
+                                        onClick={() => handlePlanetClick(index)}
                                     >{nomePlaneta}
                                         <span className={`block mt-1 w-full h-1 ${activatePlanet === index ? "bg-white" : "group-hover:bg-gray-400"}`}></span>
                                     </li>
@@ -44,22 +46,22 @@ export default function Destinos() {
                         </ul>
 
                         <div>
-                            <h1 className={`text-8xl`}>Lua</h1>
+                            <h1 className={`text-8xl`}>{dataPlanetaDestinations[activatePlanet].name}</h1>
                         </div>
                         <div>
-                            <p className="text-[#D0D6F9]">
-                                See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+                            <p className="text-[#D0D6F9] min-h-24">
+                                {dataPlanetaDestinations[activatePlanet].description}
                             </p>
                         </div>
                         <div className="h-1 w-full border-t border-[#767676]"></div>
                         <div className="flex justify-between pr-24">
                             <div>
                                 <p className="text-[#D0D6F9]">AVG. Distância</p>
-                                <p className="text-2xl">380,400K M</p>
+                                <p className="text-2xl">{dataPlanetaDestinations[activatePlanet].distance}</p>
                             </div>
                             <div>
-                                <p className="text-[#D0D6F9]">AVG. Distância</p>
-                                <p className="text-2xl">380,400K M</p>
+                                <p className="text-[#D0D6F9]">EST. tempo de viagem</p>
+                                <p className="text-2xl">{dataPlanetaDestinations[activatePlanet].travel}</p>
                             </div>
                         </div>
                     </div>
